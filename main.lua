645aadaa8afedb509351b13790093babfe1b615a
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2015-07-19 00:39:06 +0200

    Basic looping of tracks and devices in place, but performance is crap.

--- main.lua
+++ main.lua
@@ -17,268 +17,112 @@ renoise.tool():add_menu_entry {
 }
 
 function oversample()
-
-  -- beside of "stacking" views in columns and rows, its sometimes also useful
-  -- to align some parts of the views for example centered or right
-  -- this is what the view builders "horizontal_aligner" and "vertical_aligner"
-  -- building blocks are for.
-  
-  -- related to this topic, we'll also show how you can auto size views: (size
-  -- a view relative to its parents size). This is done by simply specifying
-  -- percentage values for the sizes, like: width = "100%"
-
   local vb = renoise.ViewBuilder()
 
-  local DIALOG_MARGIN = renoise.ViewBuilder.DEFAULT_DIALOG_MARGIN
-  local CONTENT_SPACING = renoise.ViewBuilder.DEFAULT_CONTROL_SPACING
+  local DEFAULT_DIALOG_MARGIN = renoise.ViewBuilder.DEFAULT_DIALOG_MARGIN
+  local DEFAULT_CONTROL_SPACING = renoise.ViewBuilder.DEFAULT_CONTROL_SPACING
+  local CONTENT_MARGIN = renoise.ViewBuilder.DEFAULT_CONTROL_MARGIN
+  local COLUMN_WIDTH = 8 * renoise.ViewBuilder.DEFAULT_CONTROL_HEIGHT
 
-  -- lets create a simple dialog as usual, and align a few totally useless
-  -- buttons & texts:
-  local dialog_content = vb:column {
-    id = "dialog_content",
-    margin = DIALOG_MARGIN,
-    spacing = CONTENT_SPACING,
-    
-    -- first center a text. We don't necessarily need aligners for this, 
-    -- but simply resize the text to fill the entrire row in the column, 
-    -- then tell the text view how it should align its content:
+  local knownDevices = {
+    "FabFilter: Pro-MB"
+  }
+
+  local knownParameters = {
+    "Oversampling",    
+    "Processing Mode",
+    "Processing Resolution"
+  }
+
+  local layout = vb:row {
+    margin = CONTENT_MARGIN
+  }
+
+  local knownDevicesRow = vb:row {
+    margin = DEFAULT_DIALOG_MARGIN,
+    spacing = DEFAULT_CONTROL_SPACING,
+
     vb:text {
-      text = "horizontal_aligner",
-      width = "100%",
-      align = "center",
-      font = "bold"
-    },
-    
-    -- add a large Textfield (this will be our largest view the other 
-    -- views will align to)
-    vb:textfield {
-      value = "Large Text field",
-      width = 300
-    },
-    
-    -- align a text to the right, using 'align_horizontal'
-    -- aligners do have margins & spacings, just like columns, rows have, which
-    -- we now use to place a text 20 pixels on the right, top, bottom:
-    vb:horizontal_aligner {
-      mode = "right",
-      margin = 20,
-
-      vb:text {
-        text = "I'm right and margined",
-      },
-    },
-    
-    -- align a set of buttons to the left, using 'align_horizontal'
-    -- well, this is actually just what "row" does, but horizontal_aligner 
-    -- automatically uses a width of "100%", while you can not use a "row"
-    -- with a relative width...
-    vb:horizontal_aligner {
-      mode = "left",
-
-      vb:button {
-        text = "[",
-        width = 40
-      },
-      vb:button {
-        text = "Left",
-        width = 80
-      },
-      vb:button {
-        text = "]",
-        width = 20
-      }
-    },
-    
-    -- align a set of buttons to the right, using 'align_horizontal'
-    vb:horizontal_aligner {
-      mode = "right",
-
-      vb:button {
-        text = "[",
-        width = 40
-      },
-      vb:button {
-        text = "Right",
-        width = 80
-      },
-      vb:button {
-        text = "]",
-        width = 20
-      }
-    },
-    
-    -- align a set of buttons centered, using 'align_horizontal'
-    vb:horizontal_aligner {
-      mode = "center",
-
-      vb:button {
-        text = "Center",
-        width = 80
-      }
-    },
-    
-    -- again a set of buttons centered, but with some spacing
-    vb:horizontal_aligner {
-      mode = "center",
-      spacing = 8,
-
-       vb:button {
-        text = "[",
-        width = 40
-      },
-      vb:button {
-        text = "Spacing = 8",
-        width = 80
-      },
-      vb:button {
-        text = "]",
-        width = 20
-      }
-    },
-    
-    -- show the "justify" align style
-    vb:horizontal_aligner {
-      mode = "justify",
-      spacing = 8,
-
-       vb:button {
-        text = "[",
-        width = 40
-      },
-      vb:button {
-        text = "Justify",
-        width = 80
-      },
-      vb:button {
-        text = "]",
-        width = 20
-      }
-    },
-    
-    -- show the "distribute" align style
-    vb:horizontal_aligner {
-      mode = "distribute",
-      spacing = 8,
-
-       vb:button {
-        text = "[",
-        width = 40
-      },
-      vb:button {
-        text = "Distribute",
-        width = 80
-      },
-      vb:button {
-        text = "]",
-        width = 20
-      }
-    },
-    
-
-    -- add a space before we start with a "new category"
-    vb:space {
-      height = 20
-    },
-    
-    -- lets use/show relative width, height properties:
-    vb:text {
-      text = "relative sizes",
-      font = "bold",
-      width = "100%",
-      align = "center"
-    },
-    
-    
-    -- create a aligner again, but this time just to stack
-    -- some views:
-    vb:horizontal_aligner {
-      width = "100%",
-      
-      vb:button {
-        text = "20%",
-        width = "20%"
-      },
-      vb:button {
-        text = "80%",
-        width = "80%"
-      },
-    },
-    
-
-    -- again a space before we start with a "new category"
-    vb:space {
-      height = 20
-    },
-    
-    -- not lets create a button that toggles another view. when toggling, we 
-    -- do update the main racks size which also updates the dialogs size: 
-    vb:text {
-      text = "resize racks & dialogs",
-      width = "100%",
-      align = "center",
-      font = "bold"
-    },
-    
-    -- add a button that hides the other view:
-    vb:button {
-      text = "Click me",
-      notifier = function()
-        -- toggle visibility of the view on each click
-        vb.views.hide_me_text.visible = not vb.views.hide_me_text.visible
-
-        -- and update the main content view size and thus also the dialog size
-        vb.views.dialog_content:resize()
-      end,
-    },
-
-    -- the text view that we are going to show/hide
-    vb:text {
-      id = "hide_me_text",
-      text = "Click the button above to hide this view",
+      id = "knownDevices",
+      text = "Known devices"
     },
   }
-  
- renoise.app():show_custom_dialog(
-    "Aligning & Auto Sizing", dialog_content)
+
+  for t = 1, table.getn(renoise.song().tracks) do
+    local track = renoise.song().tracks[t]
+    local trackColumn = vb:column {
+      vb:text {
+        text = track.name,
+        width = COLUMN_WIDTH
+      }
+    }
+    
+    -- print(track.name)
+
+    for d = 1, table.getn(track.devices) do
+      local device = track.devices[d]
+
+      trackColumn:add_child(vb:text {
+        text = device.name
+      })
+
+      -- octave_row:add_child(note_button)
+
+      -- print(("    %s (%s)"):format(device.name, device.device_path))
+
+      if (device.is_active) then
+        for p = 1, table.getn(device.parameters) do
+          local parameter = device.parameters[p]
+
+          --[[
+
+          print(("        %s: %d, min(%d), $max(%d), quantum(%d), default(%d)."):format(            
+            parameter.name,
+            parameter.value,
+            parameter.value_min,
+            parameter.value_max,
+            parameter.value_quantum,
+            parameter.value_default
+          ))
+
+          -- parameter.record_value(value)
+          ]]--
+
+        end
+      end
+    end
+  end
+
+  --[[
+  local NUM_OCTAVES = 10
+  local NUM_NOTES = 12
+
+  for octave = 1,NUM_OCTAVES do
+    -- create a row for each octave
+    local octave_row = vb:row {}
+
+    for note = 1,NUM_NOTES do
+      local note_button = vb:button {
+        width = BUTTON_WIDTH,
+        text = note_strings[note]..tostring(octave - 1),
+
+        notifier = function()
+          -- functions do memorize all values in the scope they are
+          -- nested in (upvalues), so we can simply access the note and 
+          -- octave from the loop here:
+          show_status(("note_button %s%d got pressed"):format(
+            note_strings[note], octave - 1))
+        end
+
+      }
+      -- add the button by "hand" into the octave_row
+      octave_row:add_child(note_button)
+    end
+
+    layout:add_child(octave_row)
+  end
+
+  ]]--
+
+  renoise.app():show_custom_dialog("Batch Building Views", layout, {"OK"})
 end
-
-
---[[
--- Devices.
-
-renoise.song().tracks[].available_devices[]
-  -> [read-only, array of strings]
-
--- Returns a list of tables containing more information about the devices. 
--- Each table has the following fields:
---  {
---    path,           -- The device's path used by insert_device_at()
---    name,           -- The device's name
---    short_name,     -- The device's name as displayed in shortened lists
---    favorite_name,  -- The device's name as displayed in favorites
---    is_favorite,    -- true if the device is a favorite
---    is_bridged      -- true if the device is a bridged plugin
---  }
-renoise.song().tracks[].available_device_infos[]
-  -> [read-only, array of strings]
-
-renoise.song().tracks[].devices[], _observable
-  -> [read-only, array of renoise.AudioDevice objects]
-
---------------------------------------------------------------------------------
--- renoise.GroupTrack (inherits from renoise.Track)
---------------------------------------------------------------------------------
-
--------- Functions
-
--- All member tracks of this group (including subgroups and their tracks).
-renoise.song().tracks[].members[]
-  -> [read-only, array of member tracks]
-
--- Collapsed/expanded visual appearance of whole group.
-renoise.song().tracks[].group_collapsed
-  -> [boolean]
-
-
-
-]]--
