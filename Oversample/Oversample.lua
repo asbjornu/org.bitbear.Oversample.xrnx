cece0bd5766d3df734ab9b4c7275292e66b14208
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2017-12-15 00:18:46 +0100

    Columns are finally appearing in the dialogue

--- Oversample/Oversample.lua
+++ Oversample/Oversample.lua
@@ -1,21 +1,20 @@
+local vb = renoise.ViewBuilder()
+local DEFAULT_DIALOG_MARGIN = renoise.ViewBuilder.DEFAULT_DIALOG_MARGIN
+local DEFAULT_CONTROL_SPACING = renoise.ViewBuilder.DEFAULT_CONTROL_SPACING
+local CONTENT_MARGIN = renoise.ViewBuilder.DEFAULT_CONTROL_MARGIN
+local COLUMN_WIDTH = 8 * renoise.ViewBuilder.DEFAULT_CONTROL_HEIGHT
+
+local knownDevices = {
+    "FabFilter: Pro-MB"
+}
+
+local knownParameters = {
+    "Oversampling",
+    "Processing Mode",
+    "Processing Resolution"
+}
+
 function oversample()
-    local vb = renoise.ViewBuilder()
-
-    local DEFAULT_DIALOG_MARGIN = renoise.ViewBuilder.DEFAULT_DIALOG_MARGIN
-    local DEFAULT_CONTROL_SPACING = renoise.ViewBuilder.DEFAULT_CONTROL_SPACING
-    local CONTENT_MARGIN = renoise.ViewBuilder.DEFAULT_CONTROL_MARGIN
-    local COLUMN_WIDTH = 8 * renoise.ViewBuilder.DEFAULT_CONTROL_HEIGHT
-
-    local knownDevices = {
-        "FabFilter: Pro-MB"
-    }
-
-    local knownParameters = {
-        "Oversampling",
-        "Processing Mode",
-        "Processing Resolution"
-    }
-
     local layout = vb:row {
         margin = CONTENT_MARGIN
     }
@@ -47,34 +46,34 @@ end
 function do_oversample(layout)
     print('do_oversample')
 
-    local tracks = renoise.song().tracks
-    for t = 1, #tracks do
+    for t = 1, #renoise.song().tracks do
         local track = renoise.song().tracks[t]
+
+        -- print(track.name)
+
         local trackColumn = vb:column {
+            margin = DEFAULT_DIALOG_MARGIN,
             vb:text {
                 text = track.name,
                 width = COLUMN_WIDTH
             }
         }
 
-        print(track.name)
-
-        for d = 1, table.getn(track.devices) do
+        for d = 1, #track.devices do
             local device = track.devices[d]
+
+            -- print(("    %s (%s)"):format(device.name, device.device_path))
 
             trackColumn:add_child(vb:text {
                 text = device.name
             })
-
-            -- octave_row:add_child(note_button)
-
-            print(("    %s (%s)"):format(device.name, device.device_path))
 
             if (device.is_active) then
                 local parameters = device.parameters
                 for p = 1, #parameters do
                     local parameter = device.parameters[p]
 
+                    --[[
                     print(("        %s: %d, min(%d), $max(%d), quantum(%d), default(%d)."):format(
                         parameter.name,
                         parameter.value,
@@ -84,9 +83,8 @@ function do_oversample(layout)
                         parameter.value_default
                     ))
 
-                    --[[
-            -- parameter.record_value(value)
-            ]]--
+                    -- parameter.record_value(value)
+                    ]]--
 
                     coroutine.yield()
                 end
