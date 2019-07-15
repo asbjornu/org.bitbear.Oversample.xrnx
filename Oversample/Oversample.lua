32c27e6dcad64e9566ff9fd4b60ad880947f51e6
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2019-07-16 00:22:06 +0200

    Add Oversample and Settings buttons

--- Oversample/Oversample.lua
+++ Oversample/Oversample.lua
@@ -3,20 +3,34 @@ local DEFAULT_DIALOG_MARGIN = renoise.ViewBuilder.DEFAULT_DIALOG_MARGIN
 local DEFAULT_CONTROL_SPACING = renoise.ViewBuilder.DEFAULT_CONTROL_SPACING
 local CONTENT_MARGIN = renoise.ViewBuilder.DEFAULT_CONTROL_MARGIN
 local COLUMN_WIDTH = 8 * renoise.ViewBuilder.DEFAULT_CONTROL_HEIGHT
-
-local knownDevices = {
-    "FabFilter: Pro-MB"
-}
-
-local knownParameters = {
-    "Oversampling",
-    "Processing Mode",
-    "Processing Resolution"
-}
-
-local dialog = nil
+local oversample_dialog = nil
+local settings_dialog = nil
 
 function oversample()
+    local layout = vb:row {
+        margin = CONTENT_MARGIN
+    }
+
+    local oversampleColumn = vb:column {
+        margin = DEFAULT_DIALOG_MARGIN,
+        vb:button {
+            text = "Oversample",
+            width = COLUMN_WIDTH
+        }
+    }
+    
+    oversampleColumn:add_child(vb:button {
+        text = "Settings",
+        width = COLUMN_WIDTH,
+        notifier = settings
+    })
+    
+    layout:add_child(oversampleColumn)
+
+    oversample_dialog = renoise.app():show_custom_dialog("Oversample", layout)
+end
+
+function settings()
     local layout = vb:row {
         margin = CONTENT_MARGIN
     }
@@ -26,9 +40,8 @@ function oversample()
         spacing = DEFAULT_CONTROL_SPACING,
 
         vb:text {
-            id = "knownDevices",
             text = "Known devices"
-        },
+        }
     }
 
     print('oversample:ProcessSlicer:init')
@@ -38,7 +51,7 @@ function oversample()
     slicer:start()
 
     print('oversample:Renoise:show_custom_dialog')
-    dialog = renoise.app():show_custom_dialog("Batch Building Views", layout)
+    settings_dialog = renoise.app():show_custom_dialog("Oversample settings", layout)
 
     -- print('oversample:ProcessSlicer:stop')
     -- slicer:stop()
@@ -50,7 +63,7 @@ function enumerate_tracks(layout)
     local song = renoise.song()
 
     for t = 1, table.getn(song.tracks) do
-        if (dialog and not dialog.visible) then
+        if (settings_dialog and not settings_dialog.visible) then
             print('Dialog closed, stopping.')
             return
         end
@@ -80,7 +93,7 @@ end
 
 function enumerate_devices(track, trackColumn)
     for d = 1, table.getn(track.devices) do
-        if (dialog and not dialog.visible) then
+        if (settings_dialog and not settings_dialog.visible) then
             print('Dialog closed, stopping.')
             return
         end
@@ -107,7 +120,7 @@ end
 
 function enumerate_parameters(device)
     for p = 1, table.getn(device.parameters) do
-        if (dialog and not dialog.visible) then
+        if (settings_dialog and not settings_dialog.visible) then
             print('Dialog closed, stopping.')
             return
         end
